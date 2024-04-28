3554faa196fee57458171147d7c0ef8897814d93
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-04-28 18:54:27 +0100

    builder type desync

--- play.pokemonshowdown.com/js/client-teambuilder.js
+++ play.pokemonshowdown.com/js/client-teambuilder.js
@@ -23,15 +23,10 @@
 			if (this.curTeam) {
 				this.curTeam.iconCache = '!';
 				this.curTeam.gen = this.getGen(this.curTeam.format);
-				this.curTeam.dex = Dex.forGen(this.curTeam.gen);
-				if (this.curTeam.format.includes('letsgo')) {
-					this.curTeam.dex = Dex.mod('gen7letsgo');
-				}
-				if (this.curTeam.format.includes('bdsp')) {
-					this.curTeam.dex = Dex.mod('gen8bdsp');
-				}
-				if (this.curTeam.format.includes('ifdex')) {
-					this.curTeam.dex = Dex.mod('gen7infinitefusion');
+				if (this.curTeam.format in window.Formats && window.Formats[this.curTeam.format].mod) {
+					this.curTeam.dex = Dex.mod(window.Formats[this.curTeam.format].mod);
+				} else {
+					this.curTeam.dex = Dex.forGen(this.curTeam.gen);
 				}
 				Storage.activeSetList = this.curSetList;
 			}
@@ -750,12 +745,10 @@
 			this.curTeam = teams[i];
 			this.curTeam.iconCache = '!';
 			this.curTeam.gen = this.getGen(this.curTeam.format);
-			this.curTeam.dex = Dex.forGen(this.curTeam.gen);
-			if (this.curTeam.format.includes('letsgo')) {
-				this.curTeam.dex = Dex.mod('gen7letsgo');
-			}
-			if (this.curTeam.format.includes('bdsp')) {
-				this.curTeam.dex = Dex.mod('gen8bdsp');
+			if (this.curTeam.format in window.Formats && window.Formats[this.curTeam.format].mod) {
+				this.curTeam.dex = Dex.mod(window.Formats[this.curTeam.format].mod);
+			} else {
+				this.curTeam.dex = Dex.forGen(this.curTeam.gen);
 			}
 			Storage.activeSetList = this.curSetList = Storage.unpackTeam(this.curTeam.team);
 			this.curTeamIndex = i;
@@ -1640,12 +1633,10 @@
 		changeFormat: function (format) {
 			this.curTeam.format = format;
 			this.curTeam.gen = this.getGen(this.curTeam.format);
-			this.curTeam.dex = Dex.forGen(this.curTeam.gen);
-			if (this.curTeam.format.includes('letsgo')) {
-				this.curTeam.dex = Dex.mod('gen7letsgo');
-			}
-			if (this.curTeam.format.includes('bdsp')) {
-				this.curTeam.dex = Dex.mod('gen8bdsp');
+			if (this.curTeam.format in window.Formats && window.Formats[this.curTeam.format].mod) {
+				this.curTeam.dex = Dex.mod(window.Formats[this.curTeam.format].mod);
+			} else {
+				this.curTeam.dex = Dex.forGen(this.curTeam.gen);
 			}
 			this.save();
 			if (this.curTeam.gen === 5 && !Dex.loadedSpriteData['bw']) Dex.loadSpriteData('bw');
